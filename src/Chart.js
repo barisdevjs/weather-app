d9e0b89c9425d419ad2974bf295bbc5e60cfad86
Barış SAVAŞ <barissavas17@gmail.com>
2022-02-06 18:16:32 +0300

    saved

--- src/Chart.js
+++ src/Chart.js
@@ -1,5 +1,6 @@
 import React, {  useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
+// eslint-disable-next-line no-unused-vars
 import { Chart as ChartJS} from 'chart.js/auto';
 
 
