200ddbfb3c93365b65d6f892464708829e34d387
Barış SAVAŞ <barissavas17@gmail.com>
2022-02-06 13:42:28 +0300

    backup file for Chart.js

---
+++ src/Spare.js
@@ -0,0 +1,71 @@
+import React, {  useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS} from 'chart.js/auto';
+
+// this file is backup for the Chart.js file for older versions of project
+
+const Chart = ({following}) => {
+  const date = following.map(item => new Date(item.dt * 1000));
+  const dateLocal = date.map(item => item.toLocaleString('default', { weekday: 'long' }));
+  const temp = following.map(item => Math.floor(item.temp.day));
+
+ 
+  const [userData, setUserData] = useState({ 
+    labels: dateLocal,
+    datasets: [
+      { 
+        data: temp,       
+        label: 'Temperature', 
+        backgroundColor: [
+          'rgba(0, 0, 0,.5)',
+          'rgba(0, 0, 0,.5)',
+          'rgba(0, 0, 0,.5)', 
+        ],
+        color : ['rgba(0,0,0,1)'],
+        fill:true,  
+        pointRadius: 1,
+        pointHitRadius:4,
+        pointHoverRadius:8,  
+        pointBackgroundColor: 'mediumblue',
+        borderWidth: 2,
+        pointBorderColor: 'lightblue',
+        pointBorderWidth: 2,
+        pointHoverBorderWidth: 2, 
+        tooltips: {
+          mode: 'index',
+          intersect: true,
+          windowPadding: 10,
+        },
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              fontColor: 'green'
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              display: false,
+            }, 
+            ticks: { 
+              fontColor: 'green' 
+            }
+          }] 
+        }
+      } 
+    ]   
+  });
+
+  return (
+    <div className="line" style={{ width:'100%'}}>
+    <Line  
+      data={userData} // onChange update the chart data 
+     />   
+    </div>
+  ) 
+}; 
+  
+export default Chart;
+   
